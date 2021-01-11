9bda7fb62b19f2d582aaaa73ad8e12e24f7808f6
Matthew Ricklefs <72839408+matthewricklefs@users.noreply.github.com>
2021-01-11 11:29:51 -0500

    adding dynamic styling to imitate green loading bar creating main styles within our index.css but creating dynamic styling within our index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 export default function App() {
-  const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState(7);
+
+  const styles = {
+    backgroundColor: "#00cb78",
+    width: width + "%"
+  };
 
   useEffect(() => {
     if (width < 100) {
       const interval = setInterval(() => {
         setWidth(width + 0.1);
-      }, 2);
+      }, 3);
 
       return () => clearInterval(interval);
     }
@@ -18,11 +23,11 @@ export default function App() {
 
   return (
     <div className="App">
-      <div>
+      <div style={styles}>
         <span>{width.toFixed(0)}%</span>
       </div>
 
-      <button onClick={() => setWidth(0)}>Reset</button>
+      <button onClick={() => setWidth(6)}>Reset</button>
     </div>
   );
 }
