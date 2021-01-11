0255d78f85286cd1ad3e3bdb30541e84a41e038d
Matthew Ricklefs <72839408+matthewricklefs@users.noreply.github.com>
2021-01-11 11:17:32 -0500

    using Hooks to imitate a loading screeen importing useState, useEffect, creating a useEffect function to increment percentage to set integer, setting up the button onClick to reset the width to 0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import "./styles.css";
+
+export default function App() {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    if (width < 100) {
+      const interval = setInterval(() => {
+        setWidth(width + 0.1);
+      }, 2);
+
+      return () => clearInterval(interval);
+    }
+  }, [width]);
+
+  return (
+    <div className="App">
+      <div>
+        <span>{width.toFixed(0)}%</span>
+      </div>
+
+      <button onClick={() => setWidth(0)}>Reset</button>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
